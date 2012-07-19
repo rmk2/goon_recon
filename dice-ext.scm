4f6d7c679e498600a0919a0d4503ae9ed374bfa9
Ryko <ryko@rmk2.org>
2012-07-19 15:12:17 +0100

    Added dice-ext.scm
    This is an extended version of the included dice.scm
    
    The extended dice gets rid of the unneccesarry do-loop when only one parameter
    is given to the script. Additionally, a third parameter was introduced to
    modify the die roll by the given real number, with a cond-statement added to
    check whether the third parameter (y) is zero, positive or negative and to
    print the results accordingly.

---
+++ dice-ext.scm
@@ -0,0 +1,31 @@
+#!/usr/bin/env stklos-script
+
+(require "srfi-27")
+
+(define rsrc (make-random-source))
+(random-source-randomize! rsrc)
+(define random (random-source-make-integers rsrc))
+
+(define (main arg)
+  (let ((input (cdr arg)))
+    (if (null? (cdr input))
+					;; then
+	(let ((n (string->number (car input))))
+	  (if (and (number? n) (positive? n))
+	      (print "\(d" n "\) " (+ 1 (random n)))
+	      (printerr "Input needs to be a natural number")))
+					;; else
+	(let ((n (string->number (car input))) (x (string->number (cadr input))) (y 0))
+	  (unless (null? (cddr input))
+		  (set! y (string->number (caddr input))))
+	  (if (and (number? n) (positive? n) (number? x) (positive? x) (number? y))
+	      (do ((t x (- t 1)))
+		  ((zero? t))
+		(cond
+		 ((zero? y)
+		  (print "\(d" n "\) " (+ 1 (random n))))
+		 ((positive? y)
+		  (print "\(d" n "+" y "\) " (+ 1 y (random n))))
+		 ((negative? y)
+		  (print "\(d" n y "\) " (+ 1 y (random n))))))
+	      (printerr "Input needs to be two natural numbers and an optional real number"))))))
