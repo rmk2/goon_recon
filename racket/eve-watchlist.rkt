de75cafbcd5b9698833449bb5355d3095a176852
Ryko <ryko@rmk2.org>
2015-10-16 19:27:08 +0200

    racket: added > eve-watchlist.rkt
    This script allows us to build a watchlist that can be used with Isphirel's
    watchlist importer, which requires name, ship and tag as per-entry json
    objects. The tags are rewritten based on customisable per-coalition labels,
    for which we also use our own scraped coalition data. Additionally, the script
    allows us to filter the resulting watchlist json array by name, ship or tag,
    which allows us purpose-built watchlists.

---
+++ racket/eve-watchlist.rkt
@@ -0,0 +1,95 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require json)
+(require net/url)
+
+(define coalition-tags (make-parameter #t))
+
+;; Data fetching
+
+(define data
+  (let [(file "/dev/shm/watchlist.output")]
+    (port->lines (open-input-file file))))
+
+(define coalitions
+  (let [(file "/dev/shm/coalitions.txt")]
+    (if (file-exists? file)
+	(file->lines file)
+	(call/input-url (string->url "http://eve.rmk2.org/coalitions.txt")
+			get-pure-port
+			port->lines))))
+
+;; Data handling
+
+(define-syntax input-map-split
+  (syntax-rules ()
+    ((_ input) (map (lambda (x) (string-split x ",")) input))))
+
+(define (cons-data list)
+  (map (lambda (i)
+	 (let loop ([item i] [i 0] [result '()])
+	   (cond
+	    [(and (zero? i) (< i (length item))) (loop item (+ i 1) (list* (cons 'name (list-ref item i)) result))]
+	    [(and (= i 1) (< i (length item))) (loop item (+ i 1) (list* (cons 'ship (list-ref item i)) result))]
+	    [(and (= i 2) (< i (length item)))
+	     (loop item (+ i 1) (list* (cons 'tag (rewrite-tag (find-coalition (list-ref item i)))) result))]
+	    [(and (= i 2) (>= i (length item)))
+	     (loop item (+ i 1) (list* (cons 'tag (rewrite-tag (find-coalition "None"))) result))]
+	    [else result])))
+       list))
+
+(define (hash-data)
+  (map (lambda (l) (make-hash l)) (remove-duplicates (cons-data (input-map-split data)))))
+
+;; Create an assoc-ready list of pairs for coalition data: (alliance . coalition)
+
+(define (pair-coalitions)
+  (map (lambda (x) (cons (cadr x) (car x))) (input-map-split coalitions)))
+
+;; Change coalition names to shorthand tags
+
+(define-syntax rewrite-tag
+  (syntax-rules ()
+    ((_ str) (if (coalition-tags)
+		 (case str
+		   [("Pandemic Legion") "PL"]
+		   [("The Imperium") "IMP"]
+		   [("Guardians of the Galaxy") "DUMP"]
+		   [("Drone Region Federation") "DRF"]
+		   [("Provi-Bloc") "PROVI"]
+		   [("Stain Wagon") "STAIN"]
+		   [("Red Menace") "RUS"]
+		   [("Northern Coalition.") "NC"]
+		   [else "Other"])
+		 str))))
+
+;; Find out if a given alliance is part of a coalition
+
+(define-syntax find-coalition
+  (syntax-rules ()
+    ((_ str) (if (coalition-tags)
+		 (if (not (assoc str (pair-coalitions)))
+		     str
+		     (cdr (assoc str (pair-coalitions))))
+		 str))))
+
+;; Allow us to filter (and [kind of] pretty-print) the data via regexp
+
+(define-syntax filter-results
+  (syntax-rules (:tags :alliance :hash)
+    ((_ :tags filter) (begin (coalition-tags #t) (filter-results filter)))
+    ((_ :alliance filter) (begin (coalition-tags #f) (filter-results filter)))
+    ((_ filter) (for-each (lambda (hash)
+			    (when (memf (lambda (x) (regexp-match filter x)) (hash-values hash))
+			      (println (hash-values hash))))
+			  (hash-data)))
+    ((_ :hash filter) (filter-map (lambda (hash)
+				    (if (memf (lambda (x) (regexp-match filter x)) (hash-values hash))
+					hash
+					#f))
+				  (hash-data)))))
+
+;; Exec
+
+(write-json (filter-results :hash ".*"))
