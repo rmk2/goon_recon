94140f604c2107f6262011a0d8d5c20fd549f306
Ryko <ryko@rmk2.org>
2015-11-08 23:22:40 +0100

    racket: added > eve-timers-html.rkt
    This script transforms the plaintext sov timers into a somewhat nicer html
    page, which includes jquery and jquery-tablesorter (downloaded seperately and
    in the pwd) in order to sort columns client-side.

---
+++ racket/eve-timers-html.rkt
@@ -0,0 +1,43 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require net/url)
+(require scribble/html)
+
+(define api
+  (let ([file "/var/www/servers/eve.rmk2.org/pages/sov_timers.txt"])
+    (if (file-exists? file)
+	(file->lines file)
+	(call/input-url (string->url "https://eve.rmk2.org/sov_timers.txt") get-pure-port port->lines))))
+
+
+(define-syntax input-map-split
+  (syntax-rules ()
+    ((_ input) (map (lambda (x) (string-split x ",")) input))))
+
+(define (html-output . param)
+  (output-xml (doctype 'html))
+  (output-xml
+   (html
+    (head
+     (title "Fuzzysov Timer Board")
+     (literal (style/inline type: "text/css"
+			    "td { border: 1px solid black; padding: .25em; min-width: 5em; } tr:nth-child(2n) > td { background-color:#efefef; } th { border: 1px solid black; } th.header { background: url(\"data:image/gif;base64, R0lGODlhFQAJAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAkAAAIXjI+AywnaYnhUMoqt3gZXPmVg94yJVQAAOw==\") no-repeat 99% ; margin-left: -1px; background-position: center left; padding: .2em 1.33em; text-align: left; } th.headerSortUp { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7\") no-repeat 99% ; } th.headerSortDown { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjI8Bya2wnINUMopZAQA7\") no-repeat 99% ; }"))
+     (script 'type: "text/javascript" 'src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
+     (script 'type: "text/javascript" 'src: "./jquery.tablesorter.min.js")
+     ;; (script 'type: "text/javascript" 'src: "https://raw.githubusercontent.com/christianbach/tablesorter/master/jquery.tablesorter.min.js")
+     (script (literal "$(document).ready(function() { $(\"#supers\").tablesorter( { sortList: [[7,0],[1,0]] } ); });")))
+    (body
+     (h1 "Fuzzysov Timer Board")
+     (p 'style: "padding-left:.2em" "Hint: hold down SHIFT to select multiple columns for sorting") 
+     (table 'id: "supers" 'class: "tablesorter"
+	    (thead (tr (th "Alliance")
+		       (th "Structure")
+		       (th "System")
+		       (th "Constellation")
+		       (th "Region")
+		       (th "Date")))
+	    (tbody
+	     (map (lambda (data) (tr (map (lambda (str) (td str)) data))) (input-map-split api))))))))
+
+(html-output)
