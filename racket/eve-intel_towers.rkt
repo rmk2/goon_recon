9637de58eab38490996c67d2f62aef9a28f43d17
Ryko <ryko@rmk2.org>
2015-12-02 21:01:28 +0100

    racket: modified > eve-intel_towers.rkt
    Modified script so that it now takes arbitrary groupIDs via a command-line
    argument (-g/--group), so it is no longer restricted to towers. A later commit
    will reflect this change by also changing the script name. The script also has
    toggles for the display killmail links, a list of attackers and to switch
    between the groupid as attackers or victims. Some internal functions have been
    renamed accordingly.

--- racket/eve-intel_towers.rkt
+++ racket/eve-intel_towers.rkt
@@ -11,6 +11,10 @@
 (define cl-regions (make-parameter null))
 (define cl-date (make-parameter (date->string (current-date) "~Y~m~d")))
 (define cl-end (make-parameter ""))
+(define cl-href (make-parameter #f))
+(define cl-attacker (make-parameter #f))
+(define cl-group (make-parameter 365))
+(define cl-losses (make-parameter #t))
 
 (define parse-args
   (command-line
@@ -18,23 +22,31 @@
    [("-r" "--region") str "Select regions to use in the query" (cl-regions (cons str (cl-regions)))]
    #:once-each
    [("-d" "--date") str "Select start date, format: YYYYMMDD" (cl-date str)]
-   [("-e" "--end-date") str "Select end date, format: YYYYMMDD" (cl-end str)]))
+   [("-e" "--end-date") str "Select end date, format: YYYYMMDD" (cl-end str)]
+   [("-l" "--link" "--href") "Show links to killmails, default: false" (cl-href #t)]
+   [("-a" "--attackers") "Show a list of attacking alliances, default: false" (cl-attacker #t)]
+   [("-g" "--group") str "Select a groupid, default: 365" (cl-group str)]
+   [("-k" "--kills") "Show kills by <groupid>, default: false" (cl-losses #f)]))
 
-;; Data fetching
+;; DATA fetching
 
 (define (json-api str)
   (call/input-url (string->url str) get-pure-port read-json))
 
-(define api (json-api "https://zkillboard.com/api/no-items/groupID/365/regionID/10000051/startTime/201511190000"))
-
-(define tower-typeids (json-api "https://public-crest.eveonline.com/inventory/groups/365/"))
+(define typeids (json-api (string-append "https://public-crest.eveonline.com/inventory/groups/"
+					 (if (number? (cl-group)) (number->string (cl-group)) (cl-group))
+					 "/")))
 
 (define region-ids (json-api "https://public-crest.eveonline.com/regions/"))
 
-(define (pull-url [regionid 11000001] [date (cl-date)])
+(define (pull-url [regionid 11000001] [date (cl-date)] [group (cl-group)])
   (when (number? regionid)
     (let ([built-url
-	   (string-append "https://zkillboard.com/api/groupID/365/regionID/"
+	   (string-append "https://zkillboard.com/api/"
+			  (if (cl-losses) "losses" "kills")
+			  "/groupID/"
+			  (if (number? group) (number->string group) group)
+			  "/regionID/"
 			  (if (number? regionid) (number->string regionid) regionid)
 			  "/no-items/startTime/"
 			  (if (number? date) (number->string date) date)
@@ -55,13 +67,13 @@
 
 (define-syntax filter-id
   (syntax-rules ()
-    ((_ str) (car (regexp-match #px"[0-9]{4,8}" str)))))
+    ((_ str) (car (regexp-match #px"[0-9]{3,8}" str)))))
 
 (define typeid-parse
   (map (lambda (hash) (cons
 		       (string->number (filter-id (hash-ref hash 'href)))
 		       (hash-ref hash 'name)))
-       (hash-ref tower-typeids 'types)))
+       (hash-ref typeids 'types)))
 
 (define regionid-parse
   (map (lambda (hash) (cons
@@ -73,7 +85,7 @@
   (syntax-rules (:id :name)
     ((_ :id n) (if (assoc n typeid-parse)
 		   (cdr (assoc n typeid-parse))
-		   #f))
+		   (number->string n)))
     ((_ :name str) (if (assoc str regionid-parse)
 		       (cdr (assoc str regionid-parse))
 		       #f))))
@@ -86,13 +98,21 @@
   (syntax-rules ()
     ((_ input) (map (lambda (x) (string-join x ",")) input))))
 
-(define (run-regions lst)
-  (let loop ([query lst] [i 0] [result null])
-    (if (< i (length query))
-	(loop query (+ i 1) (append result (zkill-towers (pull-url (convert-typeids :name (list-ref query i)) (cl-date)))))
-	result)))
+(define-syntax string-empty?
+  (syntax-rules ()
+    ((_ str) (if (equal? "" str)
+		 #t
+		 #f))))
 
-(define (zkill-towers url)
+(define-syntax concat-attackers
+  (syntax-rules ()
+    ((_ hash) (remove-duplicates (map (lambda (x)
+					(if (string-empty? (hash-ref x 'allianceName))
+					    "none"
+					    (hash-ref x 'allianceName)))
+				      hash)))))
+
+(define (zkill-parse url)
   (let ([parse-data url])
     (filter-map (lambda (km-list)
 		  (let ([victim (hash-ref km-list 'victim)]
@@ -100,17 +120,24 @@
 			[location (hash-ref km-list 'solarSystemID)]
 			[attackers (hash-ref km-list 'attackers)]
 			[id (hash-ref km-list 'killID)])
-		    (if (convert-typeids :id (hash-ref victim 'shipTypeID))
-			(list
-			 (convert-typeids :id (hash-ref victim 'shipTypeID))
-			 (hash-ref victim 'corporationName)
-			 (hash-ref victim 'allianceName)
-			 (solar-parse :system (number->string location))
-			 (solar-parse :region (number->string location))
-			 date
-			 (string-append "https://zkillboard.com/kill/" (number->string id) "/"))
-			#f)))
+		    (filter string?
+			    (list
+			     (convert-typeids :id (hash-ref victim 'shipTypeID))
+			     (if (string-empty? (hash-ref victim 'characterName)) #f (hash-ref victim 'characterName))
+			     (hash-ref victim 'corporationName)
+			     (hash-ref victim 'allianceName)
+			     (solar-parse :system (number->string location))
+			     (solar-parse :region (number->string location))
+			     date
+			     (if (cl-href) (string-append "https://zkillboard.com/kill/" (number->string id) "/") #f)
+			     (if (cl-attacker) (string-join (concat-attackers attackers) "|") #f)))))
 		parse-data)))
+
+(define (run-regions lst)
+  (let loop ([query lst] [i 0] [result null])
+    (if (< i (length query))
+	(loop query (+ i 1) (append result (zkill-parse (pull-url (convert-typeids :name (list-ref query i)) (cl-date) (cl-group)))))
+	result)))
 
 (for-each (lambda (x) (displayln x))
 	  (input-map-join (run-regions (cl-regions))))
