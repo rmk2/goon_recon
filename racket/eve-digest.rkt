67c766cf306b3ea08989b6500e0a9e9839494036
Ryko <ryko@rmk2.org>
2016-01-17 16:57:05 +0100

    racket: added > eve-digest.rkt
    This script will one day fetch zkill killmail data filtered by shiptype,
    alliance or region (or any combination thereof), which can then be
    turned into further digests, hopefully in the form of html output. It
    makes heavy use of eve-sql.rkt's database lookup functions and should be
    controlled via command-line switches for now, though a looping main
    construct will probably be added in the future. So far, we can poll
    data, and do some basic filtering.

---
+++ racket/eve-digest.rkt
@@ -0,0 +1,128 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require eve)
+(require "eve-sql.rkt")
+(require racket/set)
+
+;; Command-line argument handling
+
+(define cl-date (make-parameter (date->string (current-date) "~Y~m~d")))
+(define cl-end (make-parameter null))
+(define cl-href (make-parameter #f))
+(define cl-losses (make-parameter #f))
+(define cl-kills (make-parameter #t))
+(define cl-groups (make-parameter null))
+(define cl-regions (make-parameter null))
+(define cl-alliances (make-parameter null))
+
+(define parse-args
+  (command-line
+   #:multi
+   [("-r" "--region") str "Select regions to use in the query" (cl-regions (cons str (cl-regions)))]
+   [("-A" "--alliance") str "Filter by alliance ID, default: false" (cl-alliances (cons str (cl-alliances)))]
+   [("-g" "--group") str "Select a groupid, default: 365" (cl-groups (cons str (cl-groups)))]
+   #:once-each
+   [("-d" "--date") str "Select start date, format: YYYYMMDD" (cl-date str)]
+   [("-e" "--end-date") str "Select end date, format: YYYYMMDD" (cl-end str)]
+   [("-l" "--link" "--href") "Show links to killmails, default: false" (cl-href #t)]
+   #:once-any
+   [("-a" "--all") "Show kills & losses by <groupid>, default: false" (begin (cl-kills #t) (cl-losses #t))]
+   [("-k" "--kills") "Show kills by <groupid>, default: true" (begin (cl-kills #t) (cl-losses #f))]
+   [("-L" "--losses") "Show losses by <groupid>, default: false" (begin (cl-losses #t) (cl-kills #f))]))
+
+(define-syntax id/string->string
+  (syntax-rules (:map)
+    ((_ arg) (if (number? arg) (number->string arg) arg))
+    ((_ :map lst) (map (lambda (x) (id/string->string x)) lst))))
+
+(define (pull-url #:date [date (cl-date)]
+		  #:groups [groups (cl-groups)]
+		  #:regions [regions (cl-regions)]
+		  #:alliances [alliances (cl-alliances)]
+		  #:kills [show-kills? (cl-kills)]
+		  #:losses [show-losses? (cl-losses)])
+  (let ([built-url
+	 (string-append
+	  "https://zkillboard.com/api/no-items/"
+	  "startTime/" (id/string->string date) "0000"
+	  (cond
+	   [(and show-losses? show-kills?) ""]
+	   [show-losses? "/losses"]
+	   [show-kills? "/kills"]
+	   [else (error "Use either \"#:kills #t\" or \"#:losses #t\" when calling the pull-url function")])
+	  (if (not (null? groups))
+	      (string-join (map (lambda (n) (string-append "/groupID/" n)) groups) "/")
+	      "")
+	  (if (not (null? regions))
+	      (string-append "/regionID/" (string-join regions))
+	      "")
+	  (if (not (null? (cl-end))) (string-append "/endTime/" (cl-end) "0000") "")
+	  (if (not (null? alliances))
+	      (string-append "/allianceID/" (string-join alliances ","))
+	      ""))])
+    (json-api built-url)))
+
+(define (groupid->list lst)
+  (append-map (lambda (i) (map (lambda (n) (vector-ref n 0)) (parse-type :members (string->number i)))) lst))
+
+(define-syntax concat-data
+  (syntax-rules (:alliance :shiptype :check)
+    ((_ :alliance lst) (filter-map (lambda (x)
+				     (cond
+				      [(null? (cl-alliances)) x]
+				      [(member (number->string (hash-ref x 'allianceID)) (cl-alliances)) x]
+				      [else #f]))
+				   lst))
+    ((_ :shiptype lst) (filter-map (lambda (x)
+				     (cond
+				      [(null? (cl-groups)) x]
+				      [(member (hash-ref x 'shipTypeID) (groupid->list (cl-groups))) x]
+				      [else #f]))
+				   lst))
+    ((_ :check lst) (cond
+		     [(null? (cl-alliances)) (concat-data :shiptype lst)]
+		     [(null? (cl-groups)) (concat-data :alliance lst)]
+		     [else (set-intersect (concat-data :alliance lst)
+					  (concat-data :shiptype lst))]))))
+
+(define-syntax parse-helper
+  (syntax-rules ()
+    ((_ hash location date id)
+     (filter string?
+	     (list
+	      (parse-type :name (hash-ref hash 'shipTypeID))
+	      (hash-ref hash 'characterName)
+	      (hash-ref hash 'corporationName)
+	      (hash-ref hash 'allianceName)
+	      (parse-solarsystem :name location)
+	      (parse-region :name (parse-solarsystem :region location))
+	      date
+	      (if (cl-href) (string-append "https://zkillboard.com/kill/" (number->string id) "/") #f))))))
+
+(define (parse-kills lst #:attackers [run-attackers? #t])
+  (let ([km-data lst])
+    (append-map (lambda (km-list)
+		  (let ([victim (hash-ref km-list 'victim)]
+			[date (hash-ref km-list 'killTime)]
+			[location (hash-ref km-list 'solarSystemID)]
+			[attackers (hash-ref km-list 'attackers)]
+			[id (hash-ref km-list 'killID)])
+		    (filter-map (lambda (a) (parse-helper a location date id))
+				(if run-attackers?
+				    (concat-data :check attackers)
+				    (concat-data :check (list victim))))))
+		km-data)))
+
+;; Exec
+
+;; (cl-alliances '("864733958"))
+;; (cl-groups '("898"))
+;; (cl-regions (list (id/string->string (parse-region :id "Fade"))))
+
+;; (define test (pull-url #:date (cl-date) #:alliances '("864733958") #:group (parse-group :id "Black Ops") #:kills #t #:losses #t))
+(define test (pull-url))
+
+(list
+ (if (cl-kills) (parse-kills test #:attackers #t) null)
+ (if (cl-losses) (parse-kills test #:attackers #f) null))
