2aa1319c7eea863d2dfbafc8af33819921d7cbd2
Ryko <ryko@rmk2.org>
2016-02-16 22:53:08 +0100

    racket: modified > eve-sql_migrate.rkt
    fixed: avoid unnecessary variable definition of input-data/check-data
    added: prepare super-related as well as alliance-related tables
    added: fill alliance table with XML API data
    
    racket: added > eve-sql_cron.rkt
    
    This file replaces eve-sql_migrate.rkt as the regular file to run for
    updated data, since we only need to set up databases once.

--- racket/eve-sql_cron.rkt
+++ racket/eve-sql_cron.rkt
@@ -4,11 +4,5 @@
 (require eve)
 (require "eve-api_check.rkt")
 
-(sql-create-supers)
-(sql-create-alliances)
-
 (sql-replace-killmails (unique-car (input-map-split (edis-data)) second))
 (sql-replace-characters (parse-data (api-check-result (edis-list polled-data))))
-(sql-replace-alliances (api-fetch-alliances))
-
-(sql-create-view)
