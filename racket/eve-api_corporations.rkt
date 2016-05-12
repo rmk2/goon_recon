da5622c95fcd8143c5ea7d958f324d5e69d14ff4
Ryko <ryko@rmk2.org>
2016-05-12 23:59:23 +0200

    racket: added > eve-api_corporations.rkt
    Use a chain of lookups for unknown Corporations/Corporation Tickers in
    our moondata by hoping that we at least have alliance data, which we can
    feed to CREST to get a list of corporations in an alliance, which we can
    then pull from APIv2. Yuck!

---
+++ racket/eve-api_corporations.rkt
@@ -0,0 +1,60 @@
+#!/usr/bin/env racket
+#lang racket
+
+(require eve)
+
+(define (sql-update-corporations lst)
+  (for-each (lambda (x)
+	      (query sqlc "INSERT IGNORE customCorporations VALUES (?, ?, ?)"
+		     (first x)
+		     (second x)
+		     (third x)))
+	    lst))
+
+(define (query-unknown-corporations)
+  (query-rows sqlc "SELECT allianceTicker FROM moonScanView WHERE corporationName IS NULL"))
+
+(define (extract-corporationids v)
+  (filter-map (lambda (x) (cond
+			   [(sql-null? x) #f]
+			   [(eq? "-" x) #f]
+			   [else x]))
+	      (append-map vector->list v)))
+
+(define (crest-poll-allianceid id)
+  (json-api (string-append crest-root "/alliances/" (number->string id) "/")))
+
+(define (parse-corporationids lst)
+  (map (lambda (corp) (hash-ref corp 'id_str)) (hash-ref lst 'corporations)))
+
+(define (hash-poll-corporations lst)
+  (map (lambda (id) (make-hash
+		     (result->list
+		      (string->xexpr
+		       (xml-api (string-append api-root "/corp/CorporationSheet.xml.aspx?corporationID=" id))))))
+       lst))
+
+(define (hash-parse-corporations lst)
+  (map (lambda (corp) (list (hash-ref corp 'corporationID)
+			    (hash-ref corp 'ticker)
+			    (hash-ref corp 'corporationName)))
+       lst))
+
+(define (main-poll id)
+  (exec-limit-api-rate #:function hash-poll-corporations
+		       #:input (parse-corporationids (crest-poll-allianceid id))
+		       #:delay 1
+		       #:digest hash-parse-corporations
+		       #:limit 30))
+
+(define (main input)
+  (for-each (lambda (alliance)
+	      (begin
+		(log-debug (format "[debug] Writing corporations for alliance ~s to database" alliance))
+		(sql-update-corporations
+		 (main-poll (parse-alliance :id alliance)))))
+	    input))
+
+;; Exec
+
+(main (extract-corporationids (query-unknown-corporations)))
