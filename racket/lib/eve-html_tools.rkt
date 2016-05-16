e0d533d6c15702ddc3a3b564ba2336345bb70eab
Ryko <ryko@rmk2.org>
2016-05-16 15:58:18 +0200

    racket: modified > lib/eve-html_tools.rkt
    added: option to save tickers as HTML classes for further styling
    via (create-html-table), toggleable via #:ticker->class BOOLEAN
    added: option to toggle tablesorter inclusion for (create-html-head)
    added: extra fields option, which means we can simply add additional
    values to the HTML head via (create-html-head (values opt1 opt2 opt3))

--- racket/lib/eve-html_tools.rkt
+++ racket/lib/eve-html_tools.rkt
@@ -11,24 +11,26 @@
 
 (provide (prefix-out output: (all-defined-out)))
 
-(define (create-html-table input-list #:head [head null] #:id [id "main"])
+(define (create-html-table input-list #:ticker->class [ticker->class? #f] #:head [head null] #:id [id "main"])
   (div 'class: "data"
        (table 'id: id 'class: "tablesorter"
 	      (if (null? head) null (thead (tr (map th head))))
 	      (tbody (map (lambda (row)
 			    (tr
 			     (map (lambda (str)
-				    (td (cond
-					 [(sql-null? str) ""]
-					 [(sql-date? str) (date->string (sql-datetime->srfi-date str) "~1")]
-					 [(sql-timestamp? str) (date->string (sql-datetime->srfi-date str) "~1 ~3")]
-					 [(number? str) str]
-					 [(regexp-match #px"^http" str) (a 'href: str 'target: "_blank" "-> link")]
-					 [else str])))
+				    (if (and (regexp-match? #px"^[A-Z0-9. -_]{1,5}$" str) ticker->class?)
+					(td 'class: str str)
+					(td (cond
+					     [(sql-null? str) ""]
+					     [(sql-date? str) (date->string (sql-datetime->srfi-date str) "~1")]
+					     [(sql-timestamp? str) (date->string (sql-datetime->srfi-date str) "~1 ~3")]
+					     [(number? str) str]
+					     [(regexp-match #px"^http" str) (a 'href: str 'target: "_blank" "-> link")]
+					     [else str]))))
 				  row)))
 			  input-list)))))
 
-(define (create-html-head #:title [str "Title"] #:sort-column [sort-column 6])
+(define (create-html-head #:title [str "Title"] #:sort-column [sort-column 6] #:tablesorter [tablesorter? #t] [extra-fields null])
   (head
    (meta 'charset: "utf-8")
    (title str)
@@ -37,12 +39,16 @@
    (literal (style/inline 'type: "text/css" "thead { border-bottom: 1px solid black; }"))
    (literal (style/inline 'type: "text/css" "td { padding: 0.3em; border-right: 1px solid black; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 15.5em; }"))
    (literal (style/inline 'type: "text/css" "tr:nth-child(2n+1) > td { background-color: #efefef; }"))
-   (literal (style/inline 'type: "text/css" "th.header { background: url(\"data:image/gif;base64, R0lGODlhFQAJAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAkAAAIXjI+AywnaYnhUMoqt3gZXPmVg94yJVQAAOw==\") no-repeat 99% ; margin-left: -1px; background-position: center left; padding: .2em 1.33em; text-align: left; } th.headerSortUp { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7\") no-repeat 99% ; } th.headerSortDown { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjI8Bya2wnINUMopZAQA7\") no-repeat 99% ; }"))
-   (script 'type: "text/javascript" 'src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
-   (script 'type: "text/javascript" 'src: "./jquery.tablesorter.min.js")
-   (script (literal (string-append "$(document).ready(function() { $(\"*\").tablesorter( { sortList: [["
-				   (number->string sort-column)
-				   ",0]] } ); });")))))
+   (if tablesorter?
+       (begin
+	 (literal (style/inline 'type: "text/css" "th.header { background: url(\"data:image/gif;base64, R0lGODlhFQAJAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAkAAAIXjI+AywnaYnhUMoqt3gZXPmVg94yJVQAAOw==\") no-repeat 99% ; margin-left: -1px; background-position: center left; padding: .2em 1.33em; text-align: left; } th.headerSortUp { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7\") no-repeat 99% ; } th.headerSortDown { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjI8Bya2wnINUMopZAQA7\") no-repeat 99% ; }"))
+	 (script 'type: "text/javascript" 'src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
+	 (script 'type: "text/javascript" 'src: "./jquery.tablesorter.min.js")
+	 (script (literal (string-append "$(document).ready(function() { $(\"*\").tablesorter( { sortList: [["
+					 (number->string sort-column)
+					 ",0]] } ); });"))))
+       null)
+   extra-fields))
 
 (define-syntax create-html-hint
   (syntax-rules (:tablesorter :updated)
