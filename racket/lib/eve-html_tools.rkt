0ff162571434e6ae3c724b20efda938432f6d605
Ryko <ryko@rmk2.org>
2016-05-16 16:09:35 +0200

    racket: modified > lib/eve-html_tools.rkt
    fixed: use (list) instead of (begin) for #:tablesorter #t, since the
    latter only prints the last argument, while (list) passes all of them to
    the superordinate function, which is usually (head)

--- racket/lib/eve-html_tools.rkt
+++ racket/lib/eve-html_tools.rkt
@@ -40,7 +40,7 @@
    (literal (style/inline 'type: "text/css" "td { padding: 0.3em; border-right: 1px solid black; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 15.5em; }"))
    (literal (style/inline 'type: "text/css" "tr:nth-child(2n+1) > td { background-color: #efefef; }"))
    (if tablesorter?
-       (begin
+       (list
 	 (literal (style/inline 'type: "text/css" "th.header { background: url(\"data:image/gif;base64, R0lGODlhFQAJAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAkAAAIXjI+AywnaYnhUMoqt3gZXPmVg94yJVQAAOw==\") no-repeat 99% ; margin-left: -1px; background-position: center left; padding: .2em 1.33em; text-align: left; } th.headerSortUp { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7\") no-repeat 99% ; } th.headerSortDown { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjI8Bya2wnINUMopZAQA7\") no-repeat 99% ; }"))
 	 (script 'type: "text/javascript" 'src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
 	 (script 'type: "text/javascript" 'src: "./jquery.tablesorter.min.js")
