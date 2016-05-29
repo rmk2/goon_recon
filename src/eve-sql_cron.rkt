fae8acff727ef7a30605ce37a0fed086f32b5742
Ryko <ryko@rmk2.org>
2016-05-29 20:19:45 +0200

    racket: general clean-up
    removed: eve-intel.rkt (made obsolete by eve-digest.rkt)
    removed: eve-intel_filter.rkt (moved querying to database)
    removed: eve-sql_cron.rkt (eve-api_check.rkt/eve-digest.rkt can be run
    directly via cron)
    removed: eve-sql_migrate.rkt (bootstrapping is not needed)
    removed: eve-zkill_parse.rkt (made obsolete by eve-digest.rkt)
    changed: make eve-sql_init.rkt executable (chmod +x)

--- src/eve-sql_cron.rkt
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env racket
-#lang racket
-
-(require eve)
-(require "eve-api_check.rkt")
-
-(sql-replace-killmails (unique-car (input-map-split (edis-data)) second))
-(sql-replace-characters (parse-data (api-check-result (edis-list polled-data))))
