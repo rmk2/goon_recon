9e10358c10e2df13fe454261dd2275eb4fc604f9
Ryko <ryko@rmk2.org>
2016-05-29 02:43:24 +0200

    racket: added > eve-towers_sql.rkt
    This script can be used as a pipe target for eve-digest.rkt's raw output
    for control towers (--losses --g "Control Towers"), which extracts
    useful information and saves it in a database, which we then use to
    highlight towers that are offline or need to be rescanned (kill datetime
    more recent than last scan datetime) straight in the moon-database.

---
+++ racket/eve-towers_sql.rkt
@@ -0,0 +1,56 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require eve)
+
+;; Read from stdin
+
+(define pipe-input (cdr (append* (second (read)))))
+
+;; Write to database
+
+(define (sql-tower-create-raw)
+  (if (table-exists? sqlc "towerKillRaw")
+      #t
+      (query-exec sqlc "CREATE TABLE towerKillRaw ( regionID INT NOT NULL, solarSystemID INT NOT NULL, planet INT NOT NULL, moon INT NOT NULL, corporationID INT NOT NULL, allianceID INT, datetime DATETIME NOT NULL, typeID INT, killID INT, UNIQUE KEY (solarSystemID, planet, moon) )")))
+
+(define (sql-tower-update-raw lst)
+  (for-each (lambda (x)
+	      (query sqlc "INSERT INTO towerKillRaw VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE corporationID=?,allianceID=?,datetime=?,typeID=?,killID=?"
+		     (first x)
+		     (second x)
+		     (third x)
+		     (fourth x)
+		     (fifth x)
+		     (sixth x)
+		     (seventh x)
+		     (eighth x)
+		     (ninth x)
+		     (fifth x)
+		     (sixth x)
+		     (seventh x)
+		     (eighth x)
+		     (ninth x)))
+	    lst))
+
+;; Parse data into sql ready format
+
+(define (parse-tower-data lst)
+  (map (lambda (km) (let ([location (parse-map (list-ref km 7))])
+		      (flatten
+		       (list
+			(list-ref km 9)
+			(vector-ref location 3)
+			(cdr (split-moon-display (vector-ref location 6)))
+			(list-ref km 3)
+			(list-ref km 5)
+			(list-ref km 10)
+			(list-ref km 0)
+			(list-ref km 11)))))
+       lst))
+
+;; Exec
+
+(sql-tower-create-raw)
+
+(sql-tower-update-raw (parse-tower-data pipe-input))
