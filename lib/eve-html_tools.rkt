770e9e120340cb6ac926736bf2ba1f0e0054a798
Ryko <ryko@rmk2.org>
2016-06-21 02:16:39 +0200

    racket: modified > lib/eve-html_tools.rkt
    added: function to create a basic, styled navigation bar
    added: boolean navigation switch to (create-html-head)
    changed: move tablesorter-specific html-head options to own function
    added: move navigation-specific html-head options to own function

--- lib/eve-html_tools.rkt
+++ lib/eve-html_tools.rkt
@@ -43,28 +43,43 @@
 				  (drop-right row drop-amount))))
 			  input-list)))))
 
-(define (create-html-head #:title [str "Title"]
+(define (create-html-head #:title [title-str "Title"]
 			  #:sort-column [sort-column 6]
 			  #:tablesorter [tablesorter? #t]
+			  #:navigation [navigation? #f]
 			  [extra-fields null])
   (head
    (meta 'charset: "utf-8")
-   (title str)
+   (title title-str)
    (literal (style/inline 'type: "text/css" ".data { margin: 1em 0; }"))
    (literal (style/inline 'type: "text/css" "table { border-collapse: collapse;  border: 1px solid black; width: 100%; }"))
    (literal (style/inline 'type: "text/css" "thead { border-bottom: 1px solid black; }"))
    (literal (style/inline 'type: "text/css" "td { padding: 0.3em; border-right: 1px solid black; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 14em; }"))
    (literal (style/inline 'type: "text/css" "tr:nth-child(2n) { background-color: #efefef; }"))
-   (if tablesorter?
-       (list
-	(literal (style/inline 'type: "text/css" "th.header { background: url(\"data:image/gif;base64, R0lGODlhFQAJAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAkAAAIXjI+AywnaYnhUMoqt3gZXPmVg94yJVQAAOw==\") no-repeat 99% ; margin-left: -1px; background-position: center left; padding: .2em 1.33em; text-align: left; } th.headerSortUp { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7\") no-repeat 99% ; } th.headerSortDown { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjI8Bya2wnINUMopZAQA7\") no-repeat 99% ; }"))
-	(script 'type: "text/javascript" 'src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
-	(script 'type: "text/javascript" 'src: "https://eve.rmk2.org/js/jquery.tablesorter.min.js")
-	(script (literal (string-append "$(document).ready(function() { $(\"*\").tablesorter( { sortList: [["
-					(number->string sort-column)
-					",0]] } ); });"))))
-       null)
+   (if tablesorter? (create-html-head-tablesorter sort-column) null)
+   (if navigation? (create-html-head-navigation) null)
    extra-fields))
+
+(define (create-html-head-tablesorter sort-column)
+  (list
+   (literal (style/inline 'type: "text/css" "th.header { background: url(\"data:image/gif;base64, R0lGODlhFQAJAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAkAAAIXjI+AywnaYnhUMoqt3gZXPmVg94yJVQAAOw==\") no-repeat 99% ; margin-left: -1px; background-position: center left; padding: .2em 1.33em; text-align: left; } th.headerSortUp { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjB+gC+jP2ptn0WskLQA7\") no-repeat 99% ; } th.headerSortDown { background: url(\"data:image/gif;base64, R0lGODlhFQAEAIAAACMtMP///yH5BAEAAAEALAAAAAAVAAQAAAINjI8Bya2wnINUMopZAQA7\") no-repeat 99% ; }"))
+   (script 'type: "text/javascript" 'src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
+   (script 'type: "text/javascript" 'src: "https://eve.rmk2.org/js/jquery.tablesorter.min.js")
+   (script (literal (string-append "$(document).ready(function() { $(\"*\").tablesorter( { sortList: [["
+				   (number->string sort-column)
+				   ",0]] } ); });")))))
+
+(define (create-html-head-navigation)
+  (list
+   (style/inline 'type: "text/css" "body { margin: 0; }")
+   (style/inline 'type: "text/css" "#nav { width: 100%; display: inline-block; border-bottom: 1px solid black; }")
+   (style/inline 'type: "text/css" "#content { margin: 0.5em; clear: left; }")
+   (style/inline 'type: "text/css" ".nav-title { font-weight: bold; padding: 0.75em; }")
+   (style/inline 'type: "text/css" "ul { list-style-type: none; margin: 0; padding: 0; }")
+   (style/inline 'type: "text/css" "li { float: left; }")
+   (style/inline 'type: "text/css" "li:first-child { margin-right: 4em; }")
+   (style/inline 'type: "text/css" "li a { display: block; padding: 0.75em; text-decoration: none; color: black; }")
+   (style/inline 'type: "text/css" "li a:hover { background-color: lightgray; }")))
 
 (define-syntax create-html-hint
   (syntax-rules (:tablesorter :updated)
@@ -90,3 +105,12 @@
 		     (optgroup 'label: "W-Space"
 			       (map option (filter (lambda (x) (regexp-match "^[A-Z0-9]-" x)) region-list))))
 	     (input 'type: "submit"))))
+
+(define (create-html-navigation #:title [nav-title "GoonSwarm Recon"]
+				#:links [nav-list '(("Dashboard" . "report")
+						    ("Tasks" . "tasks")
+						    ("Timerboard" . "timers"))])
+  (div 'id: "nav"
+       (ul (li 'class: "nav-title" nav-title)
+	   (map (lambda (x) (li (a 'href: (cdr x) (car x))))
+		nav-list))))
