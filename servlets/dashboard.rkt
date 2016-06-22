309b93be3b8415be06054bf47638c196455c0925
Ryko <ryko@rmk2.org>
2016-06-22 19:37:17 +0200

    racket: modified > servlets/dashboard.rkt
    added: navigation bar + css styling to main
    sheets (report/result/tasks/timers), which we generate via the
    eve-html_tools.rkt library's (create-html-navigation) function

--- servlets/dashboard.rkt
+++ servlets/dashboard.rkt
@@ -147,8 +147,9 @@
        (output-xml (doctype 'html) port)
        (output-xml
 	(html
-	 (output:create-html-head #:title "Dashboard" #:tablesorter #f)
+	 (output:create-html-head #:title "Dashboard" #:tablesorter #f #:navigation #t)
 	 (body
+	  (output:create-html-navigation #:title "GoonSwarm Recon")
 	  (div 'id: "content"
 	       (h1 "Dashboard")
 	       (form 'action: "result" 'method: "POST" 'target: "_self" 'id: "main" 'novalidate: #f
@@ -185,8 +186,9 @@
        (output-xml (doctype 'html) port)
        (output-xml
 	(html
-	 (output:create-html-head #:title "Dashboard Scan Result" #:tablesorter #f)
+	 (output:create-html-head #:title "Dashboard Scan Result" #:tablesorter #f #:navigation #t)
 	 (body
+	  (output:create-html-navigation #:title "GoonSwarm Recon")
 	  (div 'id: "content"
 	       (h1 (pretty-print-location (guess->location (dscan-guess-location data))))
 	       (b "Scan Result: ")
@@ -290,12 +292,14 @@
 	 (output:create-html-head
 	  #:title "Recon Moon Scanning Tasks"
 	  #:tablesorter #t
+	  #:navigation #t
 	  #:sort-column 0
 	  (list (literal (style/inline 'type: "text/css" "tr > td[class=\"LOLTX\"], tr > td[class=\"OHGOD\"] { background-color: #4D6EFF; color: white; }"))
 		(literal (style/inline 'type: "text/css" "#bar { padding: 0.5em; float: right; }"))
 		(literal (style/inline 'type: "text/css" "td { white-space: normal; }"))
 		(literal (style/inline 'type: "text/css" "select { margin-right: 0.5em; }"))))
 	 (body
+	  (output:create-html-navigation #:title "GoonSwarm Recon")
 	  (output:create-region-filter regions)
 	  (div 'id: "content"
 	       (h1 "Recon Moon Scanning Tasks")
@@ -324,11 +328,12 @@
        (output-xml (doctype 'html) port)
        (output-xml
 	(html
-	 (output:create-html-head #:title "Fuzzysov Timer Board" #:sort-column 5
+	 (output:create-html-head #:title "Fuzzysov Timer Board" #:tablesorter #t #:navigation #t #:sort-column 5
 				  (list
 				   (literal (style/inline 'type: "text/css" "#bar { padding: 0.5em; float: right; }"))
 				   (literal (style/inline 'type: "text/css" "select { margin-right: 0.5em; }"))))
 	 (body
+	  (output:create-html-navigation #:title "GoonSwarm Recon")
 	  (output:create-region-filter regions)
 	  (div 'id: "content"
 	       (h1 "Fuzzysov Timer Board")
