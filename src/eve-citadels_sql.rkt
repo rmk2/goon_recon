5493bdf5bfc64deccb97e7d7ca06f31944580f30
Ryko <ryko@rmk2.org>
2016-10-12 01:31:35 +0200

    racket: modified > src/eve-citadels_sql.rkt
    fixed: simplify data-handling, since we don't change any killmail data
    
    racket: modified > lib/eve-sql_citadels.rkt
    
    changed: extract data directly from unchanged sql-killmail (see above)

--- src/eve-citadels_sql.rkt
+++ src/eve-citadels_sql.rkt
@@ -9,22 +9,10 @@
 		     (cond [(empty? input) (exit 0)]
 			   [else (filter sql-killmail? (car input))])))
 
-;; Parse data into sql ready format
+;; Filter invalid killmails
 
 (define (parse-citadel-data lst)
-  (filter-map (lambda (km) (if (positive? (sql-killmail-location km))
-			       (flatten
-				(list
-				 (sql-killmail-region km)
-				 (sql-killmail-system km)
-				 (sql-killmail-location km)
-				 (sql-killmail-corporationid km)
-				 (sql-killmail-allianceid km)
-				 (sql-killmail-datetime km)
-				 (sql-killmail-shiptype km)
-				 (sql-killmail-killid km)))
-			       #f))
-	      lst))
+  (filter (lambda (x) (positive? (sql-killmail-location x))) lst))
 
 ;; Exec
 
