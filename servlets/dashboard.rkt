516cf90b65d78f5f3409f20f3f24171f66cfa828
Ryko <ryko@rmk2.org>
2016-10-20 04:17:35 +0200

    racket: modified > servlets/dashboard.rkt
    This commit fully implements basic/jwt-based group auth. In order for
    this to work, we first require a user to sign in via basic auth, we then
    generate a jwt token for his valid group (read from db) and then run
    through a cascading set of URL dispatchers, whereby the user's group
    decides which dispatcher serves as a starting point before moving down
    the dispatch hierarchy from most restricted (owner/admin) to least
    restricted (public).
    
    added: intermediate group-dispatch that chooses initial dispatch
    added: group-dispatch call to (exec-auth)
    added: permission-specific cascading dispatchers
    changed: previous "-g" option to "-G" and its parameter to cl-test
    added: command-line parameter for group-auth

--- servlets/dashboard.rkt
+++ servlets/dashboard.rkt
@@ -43,7 +43,8 @@
 		     [data (auth:sql-auth-get-user (bytes->string/utf-8 user))])
 		(and (scrypt-hash? data)
 		     (auth:scrypt-check-hash data pass #:length 32))))
-	 (main-dispatch (auth-add-header req))]
+	 (cond [(cl-group) (group-dispatch (auth-add-header req))]
+	       [else (main-dispatch (auth-add-header req))])]
 	[else
 	 (response
 	  401 #"Unauthorized" (current-seconds) TEXT/HTML-MIME-TYPE
@@ -73,10 +74,46 @@
    [("management" "groups") exec-groups]
    [("management" "groups") #:method "post" exec-groups-modify]))
 
-;; Embed valid JSON X-Auth header in every request for local testing
+(define-values (admin-dispatch admin-url)
+  (dispatch-rules
+   [("management" "groups") exec-groups]
+   [("management" "groups") #:method "post" exec-groups-modify]
+   [else recon-l-dispatch]))
+
+(define-values (recon-l-dispatch recon-l-url)
+  (dispatch-rules
+   [("citadel-database") exec-citadel-database]
+   [("citadel-database") #:method "post" exec-citadel-database-delete]
+   [("goo-database") exec-goo-database]
+   [("moon-database") exec-moon-database]
+   [("input" "corporation" (integer-arg)) exec-input-corporation]
+   [("input" "corporation" (string-arg)) exec-input-corporation]
+   [else recon-dispatch]))
+
+(define-values (recon-dispatch recon-url)
+  (dispatch-rules
+   [("tasks") exec-tasks]
+   [("timers") exec-timers]
+   [else affiliate-dispatch]))
+
+(define-values (affiliate-dispatch affiliate-url)
+  (dispatch-rules
+   [("report") exec-report]
+   [("report") #:method "post" (lambda (req) (exec-result req #:persist-dscan (cl-persist)))]
+   [else public-dispatch]))
+
+(define-values (public-dispatch public-url)
+  (dispatch-rules
+   [("dscan") exec-dscan-report]
+   [("dscan") #:method "post" (lambda (req) (exec-parse-dscan req #:persist-dscan (cl-persist)))]
+   [("dscan" "intel") (send/back (redirect-to "/dscan" permanently))]
+   [("dscan" (string-arg)) exec-parse-archive]))
+
+;; Embed valid JSON X-Auth header in every request for local testing or read
+;; from basic auth groups
 
 (define (auth-add-header req)
-  (let ([group (if (string-empty? (cl-group)) "recon-l" (cl-group))])
+  (let ([group (if (string-empty? (cl-test)) "recon-l" (cl-test))])
     (cond [(not (false? (cl-json)))
 	   (struct-copy request req
 			[headers/raw (append
@@ -90,21 +127,29 @@
 					       (auth:create-token
 						#:subject (auth:sql-auth-get-user-group :name user)
 						#:username user)))
-				      (request-headers/raw req))]))]
+					(request-headers/raw req))]))]
 	  [else req])))
 
 ;; Intermediate steps, then dispatch
 
+(define (group-dispatch req)
+  (let ([user-group (auth:sql-auth-get-user-group :id (auth:try-authorization-header :username req))])
+    (cond [(>= user-group 256) (admin-dispatch req)]
+	  [(>= user-group 64) (recon-l-dispatch req)]
+	  [(>= user-group 32) (recon-dispatch req)]
+	  [(>= user-group 4) (affiliate-dispatch req)]
+	  [else (public-dispatch req)])))
+
 (define (main req)
-  (if (cl-auth)
-      (auth-dispatch req)
-      (main-dispatch (auth-add-header req))))
+  (cond [(cl-auth) (auth-dispatch req)]
+	[else (main-dispatch (auth-add-header req))]))
 
 ;; Parameters
 
 (define cl-auth (make-parameter #f))
+(define cl-group (make-parameter #f))
 (define cl-json (make-parameter #f))
-(define cl-group (make-parameter ""))
+(define cl-test (make-parameter ""))
 (define cl-port (make-parameter 8000))
 (define cl-prefix (make-parameter null))
 (define cl-persist (make-parameter #f))
@@ -116,10 +161,12 @@
    #:once-each
    [("-a" "--auth" "--basic-auth") "Use basic auth, default: false"
     (cl-auth #t)]
+   [("-g" "--group" "--group-auth") "Use basic group auth, default: false"
+    (cl-group #t)]
    [("-j" "--json" "--json-auth" "--json-test") "Create valid JSON header for ALL requests, default: false"
     (cl-json #t)]
-   [("-g" "--group" "--json-group") group "Specify JSON user group, default: none"
-    (if (string? group) (cl-group group) (cl-group))]
+   [("-G" "--test-group" "--json-group") group "Specify JSON user group, default: none"
+    (if (string? group) (cl-test group) (cl-test))]
    [("-d" "--persist-dscan" "--save-dscan") "Save raw dscans to disk, default: false"
     (cl-persist #t)]
    [("-P" "--port") port "Use specified port, default: 8000"
