151e7d10ba5f7d34cdc9e0b1a6a696ffd9ad50b8
Ryko <ryko@rmk2.org>
2017-01-30 02:25:00 +0100

    racket: modified > lib/eve-sql_supers.rkt
    This commit replaces the intelSuperLatest view with a table (as a
    pseudomaterialized view), adding a lot more fields to each entry in the
    process (IDs + Names, constellation, Tickers etc.). This new table
    serves as the basis for a new intelSuperWatchlist and for changes to
    eve-sql_canary.rkt, which will be made in a future commit.
    
    added: pseudomaterialized table intelSuperLatestMV
    changed: base intelSuperWatchlist on the new table
    added: insert/delete triggers for intelSuperLatestMV
    added: function to create triggers

--- lib/eve-sql_supers.rkt
+++ lib/eve-sql_supers.rkt
@@ -84,35 +84,47 @@
 				      "LEFT JOIN mapSolarSystems ON mapSolarSystems.solarSystemID = intelSuperRaw.solarSystemID "
 				      "LEFT JOIN mapRegions ON mapRegions.regionID = intelSuperRaw.regionID "))))
 
-(define (sql-super-create-latest)
-  (if (table-exists? sqlc "intelSuperLatest")
+(define (sql-super-create-lastest-pseudomaterialized-view)
+  (if (table-exists? sqlc "intelSuperLatestMV")
       #t
-      (query-exec sqlc (string-append "CREATE VIEW intelSuperLatest AS "
-				      "SELECT "
-				      "shipTypeID,characterID,characterName,corporationID,corporationName,allianceID,allianceName,"
-				      "eventType,killID,victimTypeID,solarSystemID,regionID,datetime,COUNT(killID) AS killCount,"
-				      "SEC_TO_TIME(ROUND(AVG(TIME_TO_SEC(time(datetime))))) AS activityAvg,"
-				      "ROUND((STDDEV_SAMP(TIME_TO_SEC(time(datetime))) / 3600),1) AS activityStd "
-				      "FROM intelSuperRaw "
-				      "GROUP BY characterID DESC "
-				      "ORDER BY characterName"))))
+      (query-exec sqlc (string-append
+			"CREATE TABLE intelSuperLatestMV "
+			"( UNIQUE KEY (characterID), KEY (corporationID), KEY (allianceID), "
+			"KEY (eventType), KEY (killCount), KEY (activityAvg), KEY (activityStd), KEY (datetime) ) "
+			"AS SELECT "
+			"ship.TypeID AS shipTypeID,ship.typeName AS shipTypeName,characterID,characterName,"
+			"corporation.corporationID,corporation.corporationTicker,corporation.corporationName,"
+			"alliance.allianceID,alliance.allianceTicker,alliance.allianceName,eventType,killID,"
+			"victim.TypeID AS victimTypeID,victim.TypeName AS victimTypeName,"
+			"solar.solarSystemID,solar.solarSystemName,constellation.constellationID,"
+			"constellation.constellationName,region.regionID,region.regionName,datetime,COUNT(killID) AS killCount,"
+			"SEC_TO_TIME(ROUND(AVG(TIME_TO_SEC(time(datetime))))) AS activityAvg,"
+			"ROUND((STDDEV_SAMP(TIME_TO_SEC(time(datetime))) / 3600),1) AS activityStd "
+			"FROM intelSuperRaw AS mv "
+			"LEFT JOIN customAlliances AS alliance ON alliance.allianceID = mv.allianceID "
+			"LEFT JOIN customCorporations AS corporation ON corporation.corporationID = mv.corporationID "
+			"LEFT JOIN mapSolarSystems AS solar ON solar.solarSystemID = mv.solarSystemID "
+			"LEFT JOIN mapConstellations AS constellation ON constellation.constellationID = solar.constellationID "
+			"LEFT JOIN mapRegions AS region ON region.regionID = mv.regionID "
+			"LEFT JOIN invTypes AS ship ON ship.typeID = mv.shipTypeID "
+			"LEFT JOIN invTypes AS victim ON victim.typeID = mv.victimTypeID "
+			"GROUP BY characterID DESC"))))
 
 (define (sql-super-create-watchlist)
   (if (table-exists? sqlc "intelSuperWatchlist")
       #t
-      (query-exec sqlc (string-append "CREATE VIEW intelSuperWatchlist AS "
-				      "SELECT "
-				      "shipTypes.typeName AS shipTypeName,api.characterID,api.characterName,"
-				      "api.corporationID,api.corporationName,api.allianceID,api.allianceName,"
-				      "eventType,killID,victimTypes.typeName AS victimTypeName,mapSolarSystems.solarSystemName,"
-				      "mapRegions.regionName,datetime,killCount,activityAvg,activityStd "
-				      "FROM intelSuperLatest as latest "
-				      "LEFT JOIN invTypes AS shipTypes ON shipTypes.typeID = latest.shipTypeID "
-				      "LEFT JOIN invTypes AS victimTypes ON victimTypes.typeID = latest.victimTypeID "
-				      "LEFT JOIN mapSolarSystems ON mapSolarSystems.solarSystemID = latest.solarSystemid "
-				      "LEFT JOIN mapRegions ON mapRegions.regionID = latest.regionID "
-				      "LEFT JOIN intelSuperAffiliations AS api ON api.characterID = latest.characterID "
-				      "ORDER BY latest.datetime"))))
+      (query-exec sqlc (string-append
+			"CREATE VIEW intelSuperWatchlist AS "
+			"SELECT "
+			"shipTypeID,shipTypeName,api.characterID,api.characterName,"
+			"corporation.corporationID,corporation.corporationTicker,corporation.corporationName,"
+			"alliance.allianceID,alliance.allianceTicker,alliance.allianceName,eventType,killID,"
+			"victimTypeID,victimTypeName,solarSystemID,solarSystemName,constellationID,"
+			"constellationName,regionID,regionName,datetime,killCount,activityAvg,activityStd "
+			"FROM intelSuperLatestMV as mv "
+			"INNER JOIN intelSuperAffiliations AS api ON api.characterID = mv.characterID "
+			"LEFT JOIN customAlliances AS alliance ON alliance.allianceID = api.allianceID "
+			"LEFT JOIN customCorporations AS corporation ON corporation.corporationID = api.corporationID "))))
 
 ;; Backwards compatibility
 
@@ -120,3 +132,45 @@
 
 (define (sql-filter-watchlist)
   (query-rows sqlc "SELECT characterName,shipTypeName,allianceName FROM intelSuperWatchlist"))
+
+;; Update triggers for intelSuperLatestMV (pseudomaterialized view)
+
+(define (sql-super-latest-create-trigger-insert)
+  (query-exec sqlc (string-append
+		    "CREATE TRIGGER insert_intelSuperLatestMV AFTER INSERT ON intelSuperRaw "
+		    "FOR EACH ROW BEGIN "
+		    "REPLACE INTO intelSuperLatestMV "
+		    "SELECT "
+		    "ship.TypeID,ship.TypeName,characterID,characterName,corporation.corporationID,corporation.corporationTicker,"
+		    "corporation.corporationName,alliance.allianceID,alliance.allianceTicker,alliance.allianceName,"
+		    "eventType,killID,victim.TypeID,victim.TypeName,solar.solarSystemID,solar.solarSystemName,"
+		    "constellation.constellationID,constellation.constellationName,region.regionID,region.regionName,datetime,"
+		    "COUNT(killID) AS killCount,"
+		    "SEC_TO_TIME(ROUND(AVG(TIME_TO_SEC(time(datetime))))) AS activityAvg,"
+		    "ROUND((STDDEV_SAMP(TIME_TO_SEC(time(datetime))) / 3600),1) AS activityStd "
+		    "FROM intelSuperRaw "
+		    "LEFT JOIN customAlliances AS alliance ON alliance.allianceID = NEW.allianceID "
+		    "LEFT JOIN customCorporations AS corporation ON corporation.corporationID = NEW.corporationID "
+		    "LEFT JOIN mapSolarSystems AS solar ON solar.solarSystemID = NEW.solarSystemID "
+		    "LEFT JOIN mapConstellations AS constellation ON constellation.constellationID = solar.constellationID "
+		    "LEFT JOIN mapRegions AS region ON region.regionID = NEW.regionID "
+		    "LEFT JOIN invTypes AS ship ON ship.typeID = NEW.shipTypeID "
+		    "LEFT JOIN invTypes AS victim ON victim.typeID = NEW.victimTypeID "
+		    "WHERE characterID = NEW.characterID "
+		    "GROUP BY characterID DESC; "
+		    "END;")))
+
+(define (sql-super-latest-create-trigger-delete)
+  (query-exec sqlc (string-append
+		    "CREATE TRIGGER delete_intelSuperLatestMV AFTER DELETE ON intelSuperRaw "
+		    "FOR EACH ROW BEGIN "
+		    "DELETE FROM intelSuperLatestMV "
+		    "WHERE intelSuperLatestMV.characterID = OLD.characterID; "
+		    "END;")))
+
+(define (sql-super-latest-create-triggers)
+  (begin
+    (query-exec sqlc "DROP TRIGGER IF EXISTS insert_intelSuperLatestMV")
+    (sql-super-latest-create-trigger-insert)
+    (query-exec sqlc "DROP TRIGGER IF EXISTS delete_intelSuperLatestMV")
+    (sql-super-latest-create-trigger-delete)))
