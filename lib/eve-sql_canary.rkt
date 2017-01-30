93320597a83d47a31505250890426ad28386d64a
Ryko <ryko@rmk2.org>
2017-01-30 02:28:43 +0100

    racket: modified > lib/eve-sql_canary.rkt
    changed: rebase canaryCorporations/canaryAlliances to new table
    changed: avoid curdate(), which keeps a query from being cached
    added: per-alliance canary view
    added: per-corporation canary view

--- lib/eve-sql_canary.rkt
+++ lib/eve-sql_canary.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
 (require db)
+(require db/util/datetime)
+(require srfi/19)
 
 (require "eve-sql_main.rkt")
 
@@ -9,21 +11,55 @@
 (define (sql-canary-create-corporations)
   (if (table-exists? sqlc "canaryCorporations")
       #t
-      (query-exec sqlc "CREATE VIEW canaryCorporations AS SELECT corporationID,corporationName,allianceID,allianceName,AVG(killcount) killAvg,STDDEV_SAMP(killcount) killStd FROM intelSuperWatchlist GROUP BY corporationID ORDER BY allianceName")))
+      (query-exec sqlc "CREATE VIEW canaryCorporations AS SELECT corporationID,corporationName,allianceID,allianceName,AVG(killcount) killAvg,STDDEV_SAMP(killcount) killStd FROM intelSuperLatestMV GROUP BY corporationID ORDER BY allianceName")))
 
 (define (sql-canary-create-alliances)
   (if (table-exists? sqlc "canaryAlliances")
       #t
-      (query-exec sqlc "CREATE VIEW canaryAlliances AS SELECT allianceID,allianceName,AVG(killcount) killAvg,STDDEV_SAMP(killcount) killStd FROM intelSuperWatchlist GROUP BY allianceID ORDER BY allianceName")))
+      (query-exec sqlc "CREATE VIEW canaryAlliances AS SELECT allianceID,allianceName,AVG(killcount) killAvg,STDDEV_SAMP(killcount) killStd FROM intelSuperLatestMV GROUP BY allianceID ORDER BY allianceName")))
 
 (define (sql-canary-get-watchlist #:show-losses [show-losses? #f] #:corporations [corporations? #f])
-  (query-rows sqlc (string-append "SELECT shipTypeName,characterName,canary.corporationName,canary.allianceName,"
-				  "solarSystemname,regionName,date(datetime),DATE_FORMAT(activityAvg,'%H:%m:%s'),activityStd "
-				  "FROM intelSuperWatchlist AS canary "
-				  (if corporations?
-				      "JOIN canaryCorporations AS stat ON canary.corporationID = stat.corporationID "
-				      "JOIN canaryAlliances AS stat ON canary.allianceID = stat.allianceID ")
-				  "WHERE killCount >= (killAvg + killStd) "
-				  "AND DATEDIFF(CURDATE(),datetime) <= 60 "
-				  (if show-losses? "" "AND eventType != 'Loss' ")
-				  "ORDER BY canary.allianceName,datetime,regionName,killCount DESC;")))
+  (let ([today (srfi-date->sql-date (current-date))])
+    (query-rows sqlc (string-append "SELECT shipTypeName,characterName,canary.corporationName,canary.allianceName,"
+				    "solarSystemName,regionName,date(datetime),DATE_FORMAT(activityAvg,'%H:%m:%s'),activityStd "
+				    "FROM intelSuperWatchlist AS canary "
+				    (if corporations?
+					"JOIN canaryCorporations AS stat ON canary.corporationID = stat.corporationID "
+					"JOIN canaryAlliances AS stat ON canary.allianceID = stat.allianceID ")
+				    "WHERE killCount >= (killAvg + killStd) "
+				    "AND datetime >= date_sub(?, INTERVAL 60 DAY) "
+				    (if show-losses? "" "AND eventType != 'Loss' ")
+				    "ORDER BY canary.allianceName,datetime,regionName,killCount DESC;")
+		today)))
+
+(define (sql-canary-create-watchlist-alliances)
+  (if (table-exists? sqlc "intelCanaryAlliances")
+      #t
+      (query-exec sqlc (string-append
+			"CREATE VIEW intelCanaryAlliances AS "
+			"SELECT "
+			"shipTypeName,characterName,corporationTicker,canary.corporationName,allianceTicker,"
+			"canary.allianceName,solarSystemName,constellationName,regionName,date(datetime) AS date,"
+			"DATE_FORMAT(activityAvg,'%H:%m:%s') AS activityAvg,activityStd "
+			"FROM intelSuperWatchlist AS canary "
+			"LEFT JOIN canaryAlliances AS stat ON canary.allianceID = stat.allianceID "
+			;; "WHERE datetime >= date_sub(curdate(), INTERVAL 60 DAY) "
+			"WHERE killCount >= (killAvg + killStd) "
+			"AND eventType != 'Loss' "
+			"ORDER BY canary.allianceName,datetime,regionName,killCount DESC;"))))
+
+(define (sql-canary-create-watchlist-corporations)
+  (if (table-exists? sqlc "intelCanaryCorporations")
+      #t
+      (query-exec sqlc (string-append
+			"CREATE VIEW intelCanaryCorporations AS "
+			"SELECT "
+			"shipTypeName,characterName,corporationTicker,canary.corporationName,allianceTicker,"
+			"canary.allianceName,solarSystemName,constellationName,regionName,date(datetime) AS date,"
+			"DATE_FORMAT(activityAvg,'%H:%m:%s') AS activityAvg,activityStd "
+			"FROM intelSuperWatchlist AS canary "
+			"LEFT JOIN canaryCorporations AS stat ON canary.corporationID = stat.corporationID "
+			;; "WHERE datetime >= date_sub(curdate(), INTERVAL 60 DAY) "
+			"WHERE killCount >= (killAvg + killStd) "
+			"AND eventType != 'Loss' "
+			"ORDER BY canary.allianceName,canary.corporationName,datetime,regionName,killCount DESC;"))))
