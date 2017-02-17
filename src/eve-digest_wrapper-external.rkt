90a6b53312e3563eb66bca432eb97a044163902d
Ryko <ryko@rmk2.org>
2017-02-17 22:40:45 +0100

    racket: modified > src/eve-digest_wrapper.rkt
    This commit rewrites eve-digest_wrapper.rkt to make use of the
    eve-digest_tools.rkt library, which allows us to retain the scripts
    recursive nature while avoiding an external function call via (system),
    which meant reloading the whole racket environment on each
    pass. Instead, we now launch racket once, and simply recurse after
    saving all entries to disk. The old functionality (calling via system)
    has been retained by moving the old script to a separate file,
    eve-digest_wrapper-external.rkt unchanged.
    
    changed: use functions from eve-digest_tools.rkt
    removed: external system call
    added: rudimentary command-line argument handling
    
    racket: added > src/eve-digest_wrapper-external.rkt
    
    added: unchanged content of previous eve-digest_wrapper.rkt (see above)

---
+++ src/eve-digest_wrapper-external.rkt
@@ -0,0 +1,52 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require eve)
+
+;; Get latest killID from zkillboard
+
+(define (zkill-get-lastest-id)
+  (apply max (map (lambda (hash) (hash-ref hash 'killID))
+		  (json-api "https://zkillboard.com/api/no-items/groupID/30,659/limit/5/"))))
+
+;; Parameters
+
+(define cl-start (make-parameter (sql-super-latest-killid)))
+(define cl-limit (make-parameter (zkill-get-lastest-id)))
+
+(define global-command "eve-digest.rkt")
+(define global-params "--all --raw --sql -g 'Supercarrier' -g 'Titan'")
+
+;; Main
+
+(define (run-digest #:limit [limit (cl-limit)] #:start [start (cl-start)] #:delay [delay 10])
+  (define (sql-super-latest-killid-mod)
+    (query-value sqlc "SELECT MAX(killID) FROM intelSuperRaw WHERE killID <= ?" limit))
+  (define (sql-super-latest-datetime-mod)
+    (query-value sqlc "SELECT MAX(datetime) FROM intelSuperRaw WHERE killID <= ?" limit))
+  (let* ([command global-command]
+	 [params global-params]
+	 [kill-id (number->string (sql-super-latest-killid-mod))]
+	 [program (string-append command " " params " -i " kill-id)])
+    (if (< (sql-super-latest-killid-mod) limit)
+	(begin
+	  (log-debug "[debug] Polling killmails")
+	  (let ([run (system program)])
+	    (if (not (false? run))
+		(begin
+		  (printf "last killID: ~a | last datetime: ~a~%"
+			  (let ([killid (sql-super-latest-killid-mod)])
+			    (if (number? killid) killid 0))
+			  (let ([datetime (sql-super-latest-datetime-mod)])
+			    (if (sql-timestamp? datetime)
+				(date->string (sql-datetime->srfi-date datetime) "~1 ~3")
+				0)))
+		  (log-debug (format "[debug] Waiting before next iteration: ~s seconds" delay))
+		  (sleep delay)
+		  (run-digest #:limit limit #:start start #:delay delay))
+		(error "Something went wrong!"))))
+	(exit 0))))
+
+;; Exec
+
+(run-digest #:limit (cl-limit) #:start (cl-start))
--- src/eve-digest_wrapper.rkt
+++ src/eve-digest_wrapper.rkt
@@ -5,48 +5,70 @@
 
 ;; Get latest killID from zkillboard
 
-(define (zkill-get-lastest-id)
+(define (zkill-get-lastest-id [groups '("30" "659")])
   (apply max (map (lambda (hash) (hash-ref hash 'killID))
-		  (json-api "https://zkillboard.com/api/no-items/groupID/30,659/limit/5/"))))
+		  (json-api (format "https://zkillboard.com/api/zkbOnly/groupID/~a/limit/5/" (string-join groups ","))))))
 
 ;; Parameters
 
-(define cl-start (make-parameter (sql-super-latest-killid)))
-(define cl-limit (make-parameter (zkill-get-lastest-id)))
+(define cl-groups (make-parameter '("30" "659")))
+(define cl-limit (make-parameter 200))
 
-(define global-command "eve-digest.rkt")
-(define global-params "--all --raw --sql -g 'Supercarrier' -g 'Titan'")
+(define cl-delay (make-parameter 10))
+
+(define cl-start (make-parameter null))
+(define cl-end (make-parameter (zkill-get-lastest-id (cl-groups))))
+
+;; Command-line argument handling
+
+(define parse-args
+  (command-line
+   #:once-each
+   [("-d" "--delay") str "Delay between iterations in seconds, default: 10"
+    (if (number? (string->number str)) (cl-delay str) (cl-delay))]
+   [("-s" "--start" "-i" "--id") str "Select start killID, default: auto"
+    (if (number? (string->number str)) (cl-start str) (cl-start))]
+   [("-l" "--limit") str "Limit the amount of killmails per poll, default: 200, max: 200"
+    (if (and (number? (string->number str)) (<= (string->number str) 200)) (cl-limit str) (cl-limit))]))
 
 ;; Main
 
-(define (run-digest #:limit [limit (cl-limit)] #:start [start (cl-start)] #:delay [delay 10])
+(define (run-digest #:groups [groups (cl-groups)]
+		    #:start [start (cl-start)]
+		    #:end [end (cl-end)]
+		    #:delay [delay (cl-delay)]
+		    #:limit [limit (cl-limit)])
   (define (sql-super-latest-killid-mod)
-    (query-value sqlc "SELECT MAX(killID) FROM intelSuperRaw WHERE killID <= ?" limit))
+    (query-maybe-value sqlc "SELECT MAX(killID) FROM intelSuperRaw WHERE killID <= ?" end))
   (define (sql-super-latest-datetime-mod)
-    (query-value sqlc "SELECT MAX(datetime) FROM intelSuperRaw WHERE killID <= ?" limit))
-  (let* ([command global-command]
-	 [params global-params]
-	 [kill-id (number->string (sql-super-latest-killid-mod))]
-	 [program (string-append command " " params " -i " kill-id)])
-    (if (< (sql-super-latest-killid-mod) limit)
-	(begin
-	  (log-debug "[debug] Polling killmails")
-	  (let ([run (system program)])
-	    (if (not (false? run))
-		(begin
-		  (printf "last killID: ~a | last datetime: ~a~%"
-			  (let ([killid (sql-super-latest-killid-mod)])
-			    (if (number? killid) killid 0))
-			  (let ([datetime (sql-super-latest-datetime-mod)])
-			    (if (sql-timestamp? datetime)
-				(date->string (sql-datetime->srfi-date datetime) "~1 ~3")
-				0)))
-		  (log-debug (format "[debug] Waiting before next iteration: ~s seconds" delay))
-		  (sleep delay)
-		  (run-digest #:limit limit #:start start #:delay delay))
-		(error "Something went wrong!"))))
-	(exit 0))))
+    (query-maybe-value sqlc "SELECT MAX(datetime) FROM intelSuperRaw WHERE killID <= ?" end))
+  (if (or (false? (sql-super-latest-killid-mod)) (< (sql-super-latest-killid-mod) end))
+      (begin
+	(log-debug "[debug] Polling killmails")
+	(let* ([id (if (null? start) (number->string (sql-super-latest-killid-mod)) start)]
+	       [data-raw (digest:poll-url #:date null #:groups groups #:kills #t #:losses #t #:id id #:limit limit)]
+	       [kills (map (lambda (km) (begin (set-sql-killmail-eventtype! km "Kill") km))
+			   (digest:parse-kills #:attackers #t #:raw #t #:groups groups data-raw))]
+	       [losses (map (lambda (km) (begin (set-sql-killmail-eventtype! km "Loss") km))
+			    (digest:parse-kills #:attackers #f #:raw #t #:groups groups data-raw))]
+	       [data (append kills losses)])
+	  (if (list? data)
+	      (begin
+		(sql-super-insert-killmails data)
+		(sql-super-populate-affiliations)
+		(printf "last killID: ~a | last datetime: ~a~%"
+			(let ([killid (sql-super-latest-killid-mod)])
+			  (if (number? killid) killid 0))
+			(let ([datetime (sql-super-latest-datetime-mod)])
+			  (if (sql-timestamp? datetime)
+			      (date->string (sql-datetime->srfi-date datetime) "~1 ~3")
+			      0)))
+		(log-debug (format "[debug] Waiting before next iteration: ~s seconds" delay))
+		(sleep delay)
+		(run-digest #:groups groups #:start null #:end end #:delay delay #:limit limit))
+	      (error "Something went wrong!"))))
+      (exit 0)))
 
 ;; Exec
 
-(run-digest #:limit (cl-limit) #:start (cl-start))
+(run-digest #:groups (cl-groups) #:start (cl-start) #:end (cl-end) #:limit (cl-limit))
